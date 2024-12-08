1e89cc998c9b5bc736a29ee5b60c4726de3f14d8
Drake Vance <drake@dev.com>
2024-12-08 18:46:49 +0800

    build when workflow_dispatch

--- packages/termux-exec/build.sh
+++ packages/termux-exec/build.sh
@@ -3,8 +3,8 @@ TERMUX_PKG_DESCRIPTION="An execve() wrapper to make /bin and /usr/bin shebangs w
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=1:1.0
-TERMUX_PKG_SRCURL=https://github.com/drake2020li/termux-exec/archive/refs/heads/master.zip
-TERMUX_PKG_SHA256=75542f2b97f813e773777e40ca4f7696cf42d969c55574f71dcbd0d7fda5cebf
+TERMUX_PKG_SRCURL=https://github.com/termux/termux-exec/archive/v${TERMUX_PKG_VERSION:2}.tar.gz
+TERMUX_PKG_SHA256=b977592f197bf3a87e8a005ea0ccefb3e144edc81d5e3dc8d1ad1a12512f4a68
 TERMUX_PKG_ESSENTIAL=true
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_EXTRA_MAKE_ARGS="TERMUX_PREFIX=${TERMUX_PREFIX} TERMUX_BASE_DIR=${TERMUX_BASE_DIR}"
